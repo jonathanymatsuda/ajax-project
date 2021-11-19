923c1964a861e11e972ba27f5e10e5a58ca96f1f
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-11-19 12:40:58 -0800

    Added dummy entries and ensured it looks good on mobile and desktop - slightly changed from figma wireframes

--- js/main.js
+++ js/main.js
@@ -17,4 +17,53 @@ function submission(event) {
   $journalForm.reset();
 }
 
+/* <li> - DOM Tree HTML
+  <div class="row text-align-center">
+    <div class="column-full">
+      <h3 class="white-text text-shadow">logs.title - logs.age</h3>
+    </div>
+  </div>
+  <div class="row text-align-center">
+    <div class="column-half">
+      <p class="white-text"><span class="red-text">From: </span>logs.distillery</p>
+    </div>
+    <div class="column-half">
+      <p class="white-text"><span class="red-text">On: </span>logs.date</p>
+    </div>
+  </div>
+  <div class="row text-align-center">
+    <div class="column-full">
+      <p class="red-text">Flavor Profile</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="column-full">
+      <input type="range" name="smokiness" id="smoky-profile" min="0" max="100" step="1">
+      <div class ="row justify-between">
+      <label for="smoky-profile" class ="smokiness-bar white-text">Sweet</label>
+      <label for="smoky-profile" class ="smokiness-bar white-text">Peaty</label>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="column-full">
+      <input type="range" name="intensity" id="intensity-profile" min="0" max="100" step="1">
+      <div class ="row justify-between">
+      <label for="intensity-profile" class ="intensity-bar white-text">Smooth</label>
+      <label for="intensity-profile" class ="intensity-bar white-text">Intense</label>
+      </div>
+    </div>
+  </div>
+  <div class="row text-align-center">
+    <div class="column-full">
+      <p class="white-text"><span class='red-text'>With tasting notes of: </span>logs.notes</p>
+    </div>
+  </div>
+  <div class="row text-align-center">
+    <div class="column-full">
+      <p class="white-text"><span class="red-text">Final Score: </span>logs.score</p>
+    </div>
+  </div>
+</li> */
+
 $journalForm.addEventListener('submit', submission);
